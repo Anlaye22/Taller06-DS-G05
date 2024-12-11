694909f400e55fdd4bc5a7136fdf4b9fa194c8e0
LUIS FERNANDO RIVERA MORA <154564202+lsferivera@users.noreply.github.com>
2024-12-11 16:38:19 -0500

    Update Empleado.java

--- Taller Refactoring/src/Empleado.java
+++ Taller Refactoring/src/Empleado.java
@@ -4,14 +4,16 @@ public class Empleado {
     private int horasTrabajadas;
     private String departamento;
     private double tarifaHora;
+    private String genero;
 
     public Empleado(){}
-    public Empleado(String nombre, double salarioBase, int horasTrabajadas, double tarifaHora, String departamento) {
+    public Empleado(String nombre, double salarioBase, int horasTrabajadas, double tarifaHora, String departamento, String genero) {
         this.nombre = nombre;
         this.salarioBase = salarioBase;
         this.horasTrabajadas = horasTrabajadas;
         this.tarifaHora = tarifaHora;
         this.departamento = departamento;
+        this.genero = genero;
     }
 
     public double calcularSalario() {
